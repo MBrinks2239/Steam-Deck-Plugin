58f7c3c03c1c65c9aeeff51f807fa7bf78d47323
Merlijn <44466724+MBrinks2239@users.noreply.github.com>
2024-11-10 10:10:42 +0100

    Small fixes

--- src/actions/switch-output.ts
+++ src/actions/switch-output.ts
@@ -5,8 +5,6 @@ import sonar from "../sonar-controller";
 export class SwitchOutput extends SingletonAction<SwitchOutputSettings> {
     private readonly sonarInstance = new sonar();
 
-    public outputs = ["test", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9", "test10"];
-
     override async onKeyDown(ev: KeyDownEvent<SwitchOutputSettings>): Promise<void> {
         const channel = ev.payload.settings.channelToChange;
         const output = ev.payload.settings.outputToChangeTo;
